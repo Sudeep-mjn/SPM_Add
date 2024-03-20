9e018bc576cfa7c6489b589f264013c32484d4da
Sudip Maharjan <84898672+Sudeep-mjn@users.noreply.github.com>
2024-03-20 10:16:05 +0545

    BCA SPM lab Exam 2024/03/20

---
+++ Add.py
@@ -0,0 +1,14 @@
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    result = add_numbers(num1, num2)
+    
+    print("The sum of", num1, "and", num2, "is:", result)
+
+if __name__ == "__main__":
+    main()
